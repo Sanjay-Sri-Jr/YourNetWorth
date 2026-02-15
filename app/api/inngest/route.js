0c2c8b8e67f4bebed48c65ff4d1fb0e847832da9
Sanjay Sri Jr <sanjaysri02082005@gmail.com>
2026-02-15 13:42:47 +0530

    arcjet_commit

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,4 +1,6 @@
 import { serve } from "inngest/next";
+import arcjet, { detectBot, shield } from "@arcjet/next";
+import { NextResponse } from "next/server";
 import { inngest } from "@/lib/inngest/client";
 import {
   processRecurringTransaction,
@@ -7,7 +9,23 @@ import {
   checkBudgetAlerts,
 } from "@/lib/inngest/function";
 
-export const { GET, POST, PUT } = serve({
+if (!process.env.ARCJET_KEY) {
+  throw new Error("ARCJET_KEY is missing");
+}
+
+const aj = arcjet({
+  key: process.env.ARCJET_KEY,
+  rules: [
+    shield({ mode: "LIVE" }),
+    detectBot({
+      mode: "LIVE",
+      allow: ["CATEGORY:SEARCH_ENGINE", "GO_HTTP"],
+    }),
+  ],
+});
+
+// Create Inngest handler
+const handler = serve({
   client: inngest,
   functions: [
     processRecurringTransaction,
@@ -16,3 +34,19 @@ export const { GET, POST, PUT } = serve({
     checkBudgetAlerts,
   ],
 });
+
+// Wrap POST
+export async function POST(req)
+{
+  const decision = await aj.protect(req);
+
+  if (decision.isDenied()) {
+    return new NextResponse("Forbidden", { status: 403 });
+  }
+
+  return handler(req);
+}
+
+// Let GET & PUT pass normally
+export const GET = handler;
+export const PUT = handler;
--- middleware.js
+++ middleware.js
@@ -1,30 +1,25 @@
-import arcjet, { createMiddleware, detectBot, shield } from "@arcjet/next";
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-import { NextResponse } from "next/server";
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+
 const isProtectedRoute = createRouteMatcher([
-  "/dashboard(.*)",
-  "/account(.*)",
-  "/transaction(.*)",
-]);
-const aj = arcjet({
-  key: process.env.ARCJET_KEY,
-  rules: [
-    shield({mode: "LIVE",}),
-    detectBot({mode: "LIVE", allow:["CATEGORY:SEARCH_ENGINE","GO_HTTP",],
-    })]
-});
-const clerk = clerkMiddleware(async (auth, req) => {
+    "/dashboard(.*)",
+    "/account(.*)",
+    "/transaction(.*)",
+])
+
+export default clerkMiddleware(async (auth, req) => {
   const { userId } = await auth();
+
   if (!userId && isProtectedRoute(req)) {
     const { redirectToSignIn } = await auth();
     return redirectToSignIn();
-  }
-  return NextResponse.next();
+}  
 });
-export default createMiddleware(aj, clerk);
+
 export const config = {
-  matcher: [
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    "/(api|trpc)(.*)",
-  ],
+    matcher: [
+        // Skip Next.js internals and all static files, unless found in search params
+        '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+        // Always run for API routes
+        '/(api|trpc)(.*)',
+    ],
 };
